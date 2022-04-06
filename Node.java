d844d46be645de5f0ae2bc1c8643b659e9531df9
Reiner Hüchting <reiner.huechting@gmx.de>
2022-04-06 23:48:03 +0200

    Knoten erwarten jetzt auch ein Datenelement im Konstruktor und merken es sich.

--- Node.java
+++ Node.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+
 public class Node<DataType> {
+    private DataType data;
     private List<Node<DataType>> neighbours;
 
-    public Node() {
+    public Node(DataType data) {
+        this.data = data;
         neighbours = new ArrayList<>();
     }    
 }
