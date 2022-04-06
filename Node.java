1824b8d15e6c2b26ef73c726ea65cfcac1d5c9b0
Reiner Hüchting <reiner.huechting@gmx.de>
2022-04-06 23:51:05 +0200

    Versehentlich eingebautes import entfernt.

--- Node.java
+++ Node.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-
 public class Node<DataType> {
     private DataType data;
     private List<Node<DataType>> neighbours;
