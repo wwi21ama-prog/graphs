ed9e733d79e11784d0061c5ff24213f6b10afb8a
Reiner Hüchting <reiner.huechting@gmx.de>
2022-04-06 23:33:27 +0200

    Klasse für Knoten hinzugefügt.

--- Graph.java
+++ Graph.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
-    
+    private List<Node> nodes;
+
+    public Graph() {
+        nodes = new ArrayList<>();
+    }
 }
