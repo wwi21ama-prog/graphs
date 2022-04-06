609539fbc52902507273104924f469f04c65d3ce
Reiner Hüchting <reiner.huechting@gmx.de>
2022-04-06 23:35:52 +0200

    Knoten enthalten jetzt eine Liste von Nachbarn.

--- Node.java
+++ Node.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
-    
+    private List<Node> neighbours;
+
+    public Node() {
+        neighbours = new ArrayList<>();
+    }    
 }
