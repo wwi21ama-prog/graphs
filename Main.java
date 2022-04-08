0c98434211b95d7b56d173603ffbac988cd62574
Reiner Hüchting <reiner.huechting@gmx.de>
2022-04-08 11:08:30 +0200

    Weiteres Beispiel für einen Graphen mit Zahlen als Knoten hinzugefügt.

--- Main.java
+++ Main.java
@@ -9,5 +9,19 @@ public class Main {
         g1.connect("A", "C");
 
         System.out.println(g1);
+        
+        Graph<Integer> g2 = new Graph<>();
+        
+        g2.add(42);
+        g2.add(23);
+        g2.add(57);
+        g2.add(38);
+        
+        g2.connect(42, 23);
+        g2.connect(42, 57);
+        g2.connect(57, 38);
+        g2.connect(23, 38);
+
+        System.out.println(g2);
     }
 }
